a6613ce25383dad9471ae0049a517e02e4d334d5
safaleks <safaleks@mail.ru>
2022-09-09 22:48:39 +0300

    Условия

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+        int startBalance = 120;
+        int replenishment = 980;
+        int minSumForBonus = 500;
+        int stepBonus = 150;
+
+        if (replenishment >= minSumForBonus) {
+            System.out.println("Текущий баланс - "+(startBalance+replenishment+replenishment/stepBonus) + " рублей");
+            System.out.println("Бонусные рубли - "+(replenishment/stepBonus));
+        } else {
+            System.out.println("Текущий баланс - "+(startBalance+replenishment) + " рублей");
+            System.out.println("Бонусные рубли - 0");
+        }
+    }
 }
