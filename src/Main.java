b2194c3e22fd892595cc78ee8c96f3b99b7388cf
safaleks <safaleks@mail.ru>
2022-09-11 21:03:30 +0300

    Условия: 2 итерация

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         int startBalance = 120;
-        int replenishment = 980;
-        int minSumForBonus = 500;
-        int stepBonus = 150;
+        int replenishment = 1001;
+        int minSumForBonus = 1000;
+        int stepBonus = 100;
 
-        if (replenishment >= minSumForBonus) {
+        if (replenishment > minSumForBonus) {
             System.out.println("Текущий баланс - "+(startBalance+replenishment+replenishment/stepBonus) + " рублей");
             System.out.println("Бонусные рубли - "+(replenishment/stepBonus));
         } else {
